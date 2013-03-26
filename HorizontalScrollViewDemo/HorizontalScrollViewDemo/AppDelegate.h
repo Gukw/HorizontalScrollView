e102d693444ee9591e980cbaf38594d5c69ed457
gukw <gkw521@qq.com>
2013-03-26 12:27:43 +0800

    初始化工程

---
+++ HorizontalScrollViewDemo/HorizontalScrollViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HorizontalScrollViewDemo
+//
+//  Created by Gukw on 3/26/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
