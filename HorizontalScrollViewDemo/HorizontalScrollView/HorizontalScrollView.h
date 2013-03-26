c09d139aa3a7bda485d7f21caa6c93e859d51cfc
gukw <gkw521@qq.com>
2013-03-26 12:31:01 +0800

    创建文件目录

---
+++ HorizontalScrollViewDemo/HorizontalScrollView/HorizontalScrollView.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalScrollView.h
+//  HorizontalScrollViewDemo
+//
+//  Created by Gukw on 3/26/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalScrollView : UIView
+
+@end
---
+++ HorizontalScrollViewDemo/HorizontalScrollView/HorizontalScrollView.m
@@ -0,0 +1,31 @@
+//
+//  HorizontalScrollView.m
+//  HorizontalScrollViewDemo
+//
+//  Created by Gukw on 3/26/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import "HorizontalScrollView.h"
+
+@implementation HorizontalScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
